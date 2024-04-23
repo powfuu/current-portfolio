a43ffd0504d63c132db0fe1dff397f8d14fc390b
EMOLLP4 <emolero@profile.es>
2024-04-23 09:46:58 +0200

    Se agrega loading screen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,22 @@
-import { Component, HostListener, NgZone } from '@angular/core';
+import { Component, HostListener, NgZone, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  loadingScreen: boolean = true;
   mouseX: number = 0;
   mouseY: number = 0;
 
   constructor(private ngZone: NgZone) {}
+
+  ngOnInit() {
+    setTimeout(() => {
+      this.loadingScreen = false;
+    }, 1700);
+  }
 
   @HostListener('document:mousemove', ['$event'])
   onMouseMove(event: MouseEvent) {

b8468ebeadd14067e5a4600e8e7778f96d05cec1
powfuu <everitjhon@gmail.com>
2024-04-23 18:44:13 +0200

    Set dark theme default

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component, HostListener, NgZone, OnInit } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  theme: string = 'dark';
+  theme: string = 'light';
   loadingScreen: boolean = true;
   mouseX: number = 0;
   mouseY: number = 0;

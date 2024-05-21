cfdecd8dd3f76df0d6481c09a9edbd4be992c85f
powfuu <everitjhon@gmail.com>
2024-05-21 12:57:45 +0200

    nueva release

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-
 import { importProvidersFrom } from '@angular/core';
 import { AppComponent } from './app/app.component';
 import { FormsModule } from '@angular/forms';
@@ -9,6 +8,12 @@ import { AppRoutingModule } from './app/app-routing.module';
 import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
 
 bootstrapApplication(AppComponent, {
-    providers: [importProvidersFrom(BrowserModule, AppRoutingModule, BaseModule, FormsModule)]
-})
-  .catch((err) => console.error(err));
+  providers: [
+    importProvidersFrom(
+      BrowserModule,
+      AppRoutingModule,
+      BaseModule,
+      FormsModule
+    ),
+  ],
+}).catch((err) => console.error(err));

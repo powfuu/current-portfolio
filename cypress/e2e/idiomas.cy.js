e8d1d8b3a6f3546f3f5d54b26371437d2d9960ec
powfuu <everitjhon@gmail.com>
2024-05-21 16:19:52 +0200

    se mejora prueba

--- cypress/e2e/idiomas.cy.js
+++ cypress/e2e/idiomas.cy.js
@@ -1,13 +1,18 @@
-describe("idiomas", () => {
-  it("validar idiomas", () => {
-    // en ingles
+describe("validar idiomas", () => {
+  beforeEach(() => {
     cy.visit("http://localhost:4200/#about");
-    cy.get(".language-select").select("en");
     cy.get(".flag").should("be.visible");
-    cy.get(".language-select").contains("en");
+  });
+
+  it("validar idioma es", () => {
     //en español
     cy.get(".language-select").select("es");
-    cy.get(".flag").should("be.visible");
     cy.get(".language-select").contains("es");
+  });
+
+  it("validar idioma en", () => {
+    // en ingles
+    cy.get(".language-select").select("en");
+    cy.get(".language-select").contains("en");
   });
 });

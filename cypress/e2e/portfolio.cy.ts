bf29a8eacd6a8f9591d83b8c43bd946637437e4a
Fabiana Herrera <janaherrera66@gmail.com>
2024-05-21 15:07:46 +0200

    Darle una descripcion a las pruebas end to end

--- cypress/e2e/portfolio.cy.ts
+++ cypress/e2e/portfolio.cy.ts
@@ -1,5 +1,5 @@
-describe('template spec', () => {
-  it('passes', () => {
+describe('Prueba de portfolio', () => {
+  it('Visitar localhost con puerto 4200', () => {
     cy.visit('http://localhost:4200/');
   });
 });

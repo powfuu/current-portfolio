298144609fcd350a0dd026a8fef7461decc06182
powfuu <everitjhon@gmail.com>
2024-05-21 15:35:20 +0200

    add skeleton

--- src/app/domain/pages/base/base.component.ts
+++ src/app/domain/pages/base/base.component.ts
@@ -24,6 +24,7 @@ import { SidebarComponent } from '../../shared/components/sidebar/sidebar.compon
 })
 export class BaseComponent implements OnInit {
   aboutContent$!: Observable<string>;
+  skeleton: number[] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
   constructor(
     private utilService: UtilService,

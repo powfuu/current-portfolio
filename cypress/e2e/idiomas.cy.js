c51a1993426310c9984fe4fa54400c4d7328f4e6
Fabiana Herrera <janaherrera66@gmail.com>
2024-05-22 09:39:18 +0200

    fix develop

---
+++ cypress/e2e/idiomas.cy.js
@@ -0,0 +1,18 @@
+describe("validar idiomas", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:4200/#about");
+    cy.get(".flag").should("be.visible");
+  });
+
+  it("validar idioma es", () => {
+    //en español
+    cy.get(".language-select").select("es");
+    cy.get(".language-select").contains("es");
+  });
+
+  it("validar idioma en", () => {
+    // en ingles
+    cy.get(".language-select").select("en");
+    cy.get(".language-select").contains("en");
+  });
+});
